cd5e09d8263fddf5f83ae286666aab493b6b8a72
Adriajet <steveotu77@gmail.com>
2026-09-12 09:27:33 +0100

    Add files via upload

---
+++ bill.py
@@ -0,0 +1,8 @@
+
+
+bill_amount = float(input("Enter the bill amount: "))
+paid_amount = float(input("Enter the amount paid: "))
+
+due_amount = bill_amount - paid_amount
+
+print("Customer due amount:", due_amount)
